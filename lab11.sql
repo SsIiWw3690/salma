b4f544bb739d814a565ab805809c6b2cc8822411
SsIiWw3690 <Salmaihab2002@gmail.com>
2024-11-15 18:39:59 -0500

    change the  code

--- lab11.sql
+++ lab11.sql
@@ -1,2 +1,5 @@
-SELECT course-id, title, due-date
-From assingment COMP1234 
+SELECT title,course_id,status,due_date
+FROM assingments
+WHERE  course_id like 'COMP1234%'
+And due_date < '2024-12-31'
+ORDER BY due_date;
