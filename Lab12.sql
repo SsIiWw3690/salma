9c0f7257f999dab0104343a1f7ca380e4149198c
SsIiWw3690 <Salmaihab2002@gmail.com>
2024-11-24 14:06:26 -0500

    change

---
+++ Lab12.sql
@@ -0,0 +1,8 @@
+select upper (course_name) from course
+select title,due _date
+from assigments
+where due _date like '2024-10%';
+select count (staues)from assigments group by stauess
+select max (due_date)
+from assigments
+where staues like 'completed%';
